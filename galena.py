835dc94f0084dce93a7c9858e64d3f24f2adb9d6
Anushree Jha <anushreejha0604@gmail.com>
2023-12-30 16:44:34 +0530

    Updated code

--- galena.py
+++ galena.py
@@ -1,185 +1,105 @@
-import speech_recognition as sr 
-import pyaudio 
-import pyttsx3 
-import webbrowser 
+import speech_recognition as sr
+from transformers import pipeline
+import webbrowser
 import datetime
-import time
-import calendar
-import requests
-from bs4 import BeautifulSoup
-import wikipedia as wp
-import pywhatkit as kt
-import ecapture as ecap
-
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+from gtts import gTTS
+import os
+import subprocess
+import platform
 
 def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
+    tts = gTTS(text=audio, lang='en')
+    tts.save("output.mp3")
+    os.system("afplay output.mp3")  # Adjust this command (based on your OS) for playing the audio
 
-r = sr.Recognizer()
-my_mic = sr.Microphone(device_index=0)
-with my_mic as source:
-    speak("Hello! I am Galena, your voice assistant. How may I help you?")
-    r.adjust_for_ambient_noise(source) 
-    audio = r.listen(source) 
-try: 
-        print("I heard: ")
-        print(r.recognize_google(audio)) 
-        task = r.recognize_google(audio)
-except:
-        r = sr.Recognizer()
-        my_mic = sr.Microphone(device_index=0)
-        with my_mic as source:
-                speak("I'm sorry, I did not understand. Please can you repeat what you said?")
-                r.adjust_for_ambient_noise(source) 
-                audio = r.listen(source) 
-        try: 
-                print("I heard: ")
-                print(r.recognize_google(audio)) 
-                task = r.recognize_google(audio)
-        except:
-                speak("I'm sorry, I did not understand. Please re-run the program and try again.")
+def open_app(app_name):
+    system = platform.system().lower()
+    if system == "windows":
+        subprocess.Popen(f'explorer shell:appsFolder\\{app_name}')
+    elif system == "darwin":
+        subprocess.Popen(["/usr/bin/open", "-n", f"/Applications/{app_name}.app"])
+    elif system == "linux":
+        subprocess.Popen([f"/usr/bin/{app_name}"])
+    else:
+        speak("Sorry, I cannot open apps on your operating system.")
 
 def tell_time():
-    current_time = time.strftime("%I:%M %p")
-    speak("The current time is")
-    speak(current_time)
- 
-def tell_date():
-    d = datetime.datetime.now()
-    day = d.day
-    m = d.month
-    year = d.year
-    month = calendar.month_name[m]
-    speak("Today is")
-    speak(month)
-    speak(day)
-    speak(year)
-    
-def timesofindia():
-    url = "https://timesofindia.indiatimes.com/india/timestopten.cms"
-    page_request = requests.get(url)
-    data = page_request.content
-    soup = BeautifulSoup(data,"html.parser")
+    now = datetime.datetime.now()
+    current_time = now.strftime("%I:%M %p")
+    speak(f"The current time is {current_time}")
 
-    headlines = soup.find('div').find_all('a', {'class': 'news_title'})
-    for x in headlines:
-        speak(x.text.strip())
-        
-def wikipedia():
-        query = "wiki"
-        speak(wp.search(wiki), results = 15)
-        
-def googlesearch():
-        target = look_for
-        result = kt.search(target)
-        speak(result)
-                
-while(1):
-        if 'Google' in task:
-                r = sr.Recognizer()
-                my_mic = sr.Microphone(device_index=0)
-                with my_mic as source:
-                        speak("Sure. What would you like me to search on Google?")
-                        r.adjust_for_ambient_noise(source) 
-                        audio = r.listen(source) 
-                try: 
-                        print("I heard: ")
-                        print(r.recognize_google(audio)) 
-                        look_for = r.recognize_google(audio)
-                        speak("Searching on Google. The results are:")
-                        googlesearch()
-                except:
-                        r = sr.Recognizer()
-                        my_mic = sr.Microphone(device_index=0)
-                        with my_mic as source:
-                                speak("I'm sorry, I did not understand. Please can you repeat what you said?")
-                                r.adjust_for_ambient_noise(source) 
-                                audio = r.listen(source) 
-                        try: 
-                                print("I heard: ")
-                                print(r.recognize_google(audio)) 
-                                look_for = r.recognize_google(audio)
-                                speak("Searching on Google. The results are:")
-                                googlesearch()
-                        except:
-                                speak("I'm sorry, I did not understand. Please re-run the program and try again.")
-        elif 'open YouTube' in task:
-                speak("Opening YouTube in your browser.")
-                webbrowser.open("youtube.com")
-        elif 'open Gmail' in task:
-                speak("Opening Gmail in your browser.")
-                webbrowser.open("https://mail.google.com/mail/u/0/#inbox")
-        elif 'time' in task:
-                tell_time()
-        elif 'date' in task:
-                tell_date()
-        elif 'news' in task:
-                speak("Sure. These are the top 10 news headlines from Times of India.")
-                timesofindia()
-                speak("For more information, please visit the website of Times of India.")
-        elif 'Wikipedia' in task:
-                r = sr.Recognizer()
-                my_mic = sr.Microphone(device_index=0)
-                with my_mic as source:
-                        speak("Sure. What would you like me to search on wikipedia?")
-                        r.adjust_for_ambient_noise(source) 
-                        audio = r.listen(source) 
-                try: 
-                        print("I heard: ")
-                        print(r.recognize_google(audio)) 
-                        wiki = r.recognize_google(audio)
-                        speak("Searching on Wikipedia. The results are:")
-                        wikipedia()
-                except:
-                        r = sr.Recognizer()
-                        my_mic = sr.Microphone(device_index=0)
-                        with my_mic as source:
-                                speak("I'm sorry, I did not understand. Please can you repeat what you said?")
-                                r.adjust_for_ambient_noise(source) 
-                                audio = r.listen(source) 
-                        try: 
-                                print("I heard: ")
-                                print(r.recognize_google(audio)) 
-                                wiki = r.recognize_google(audio)
-                                speak("Searching on Wikipedia. The results are:")
-                                wikipedia()
-                        except:
-                                speak("I'm sorry, I did not understand. Please re-run the program and try again.")
-        elif 'can you' or 'do' in task:
-                speak("I can tell you the time and date, search on Google and Wikipedia, open YouTube and Gmail, tell you the news from Times of India and click a photograph. If there is anything else you want me to do, please reach out to my creator. Here is the link to her GitHub profile repository.")
-                print("https://github.com/anushreejha/Galena_The-Voice-Assistant.git")
-        elif 'bye' or 'later' in task:
-                speak("Bye! I hope you have a good day!")
-                exit()
-        elif 'capture' or 'photo' in task:
-                speak("Sure! Capturing a photo. Smile please!")
-                ecap.capture(0,"capture", "image.jpg")
-        else:
-                speak("I'm sorry, I am not programmed to do that. Please try something else.")
-        r = sr.Recognizer()
-        my_mic = sr.Microphone(device_index=0)
-        with my_mic as source:
-                speak("What would you like me to do next?")
-                r.adjust_for_ambient_noise(source) 
-                audio = r.listen(source) 
-        try: 
-                print("I heard: ")
-                print(r.recognize_google(audio)) 
-                task = r.recognize_google(audio)
-        except:
-                r = sr.Recognizer()
-                my_mic = sr.Microphone(device_index=0)
-                with my_mic as source:
-                        speak("I'm sorry, I did not understand. Please can you repeat what you would like me to do next?")
-                        r.adjust_for_ambient_noise(source) 
-                        audio = r.listen(source) 
-                try: 
-                        print("I heard: ")
-                        print(r.recognize_google(audio)) 
-                        task = r.recognize_google(audio)
-                except:
-                        speak("I'm sorry, I did not understand. Please re-run the program and try again.")
+def tell_day():
+    now = datetime.datetime.now()
+    day = now.strftime("%A")
+    speak(f"Today is {day}")
+
+def google_search(query):
+    speak("Searching on Google")
+    webbrowser.open(f"https://www.google.com/search?q={query}")
+
+def bye():
+    speak("Goodbye! Have a nice day!")
+    exit(0)
+
+nlp = pipeline("sentiment-analysis")
+
+def process_query(query):
+    if "open" in query:
+        app_name = query.split("open", 1)[1].strip()
+        open_app(app_name)
+        speak("App opened")
+    elif "time" in query:
+        tell_time()
+    elif "day" in query:
+        tell_day()
+    elif "search" in query:
+        search_query = query.split("search", 1)[1].strip()  
+        google_search(search_query)
+    elif "bye" or "end" in query:
+        bye()
+    else:
+        speak("Sorry, I cannot handle that request.")
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+def ask_anything_else():
+    with mic as source:
+        speak("Is there anything else I can help you with?")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+    try:
+        user_input = r.recognize_google(audio)
+        return user_input.lower()
+    except sr.UnknownValueError:
+        speak("Sorry, I couldn't understand what you said.")
+    except sr.RequestError:
+        speak("Sorry, there was an error processing your request.")
+
+speak("Hello! How may I help you?")
+
+while True:
+    attempts = 0
+    while attempts < 2:
+        with mic as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+        try:
+            user_input = r.recognize_google(audio)
+            print(f"\nRecognized: {user_input}")  
+            process_query(user_input.lower())
+            response = ask_anything_else()
+            if "bye" in response or "no" in response:
+                speak("Goodbye!")
+                break
+        except sr.UnknownValueError:
+            speak("Sorry, I couldn't understand what you said. Can you repeat?")
+        except sr.RequestError:
+            speak("Sorry, there was an error processing your request.")
+        attempts += 1
+
+    if attempts == 2:
+        response = ask_anything_else()
+        if "bye" in response or "no" in response:
+            bye()
+            break
