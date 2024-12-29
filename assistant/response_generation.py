3f69a6c1ffa1cfcae6adbb1d45ad7dffe040121e
Anushree Jha <anushreejha0604@gmail.com>
2024-12-30 01:37:49 +0530

    Feat: News headlines

--- assistant/response_generation.py
+++ assistant/response_generation.py
@@ -3,7 +3,7 @@ import os
 
 def speak(audio):
     """
-    This function converts the provided text (audio) to speech, saves it as an MP3 file and plays the audio using the default system player.
+    This function converts the text to speech, saves it as an MP3 file, and plays it using the system's default audio player.
     """
     tts = gTTS(text=audio, lang='en')
     tts.save("output.mp3")
